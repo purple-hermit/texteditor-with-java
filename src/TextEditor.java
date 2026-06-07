b7b315a60c7c772c4f65d4d6db359fba36e34bdc
purple-hermit <218411139+purple-hermit@users.noreply.github.com>
2026-06-07 13:42:31 +0530

    Added a button to change font color

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -2,6 +2,8 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JColorChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
@@ -17,6 +19,7 @@ public class TextEditor extends JFrame implements ActionListener{
     JScrollPane scrollPane;
     JLabel fontLabel;
     JSpinner fontSizeSpinner;
+    JButton fontColorButton;
 
     TextEditor(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -49,14 +52,23 @@ public class TextEditor extends JFrame implements ActionListener{
 
         });
         
+        fontColorButton = new JButton("Color");
+        fontColorButton.addActionListener(this);
+
         this.add(fontLabel);
         this.add(fontSizeSpinner);
+        this.add(fontColorButton);
         this.add(scrollPane);
         this.setVisible(true);
     }
 
      @Override
     public void actionPerformed(ActionEvent e){
-        //TODO Auto-generated method stub
+        if(e.getSource() == fontColorButton){
+
+            Color color = JColorChooser.showDialog(null, "Choose a color", Color.black);
+
+            textArea.setForeground(color);
+        }
     }
 }
