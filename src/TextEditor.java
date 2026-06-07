83ab7b6ef9577294cb0f3453acb823f816ad3d44
purple-hermit <218411139+purple-hermit@users.noreply.github.com>
2026-06-07 16:02:18 +0530

    Added a MenuBar with a File Dropdown to Save and Open files

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -1,18 +1,28 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 import javax.swing.JButton;
 import javax.swing.JColorChooser;
 import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
 import javax.swing.JTextArea;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class TextEditor extends JFrame implements ActionListener{
 
@@ -22,6 +32,12 @@ public class TextEditor extends JFrame implements ActionListener{
     JSpinner fontSizeSpinner;
     JButton fontColorButton;
     JComboBox<String> fontBox;
+
+    JMenuBar menuBar;
+    JMenu fileMenu;
+    JMenuItem openItem;
+    JMenuItem saveItem;
+    JMenuItem exitItem;
 
     TextEditor(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -62,6 +78,26 @@ public class TextEditor extends JFrame implements ActionListener{
         fontBox.addActionListener(this);
         fontBox.setSelectedItem("Monospaced");
 
+        //Menu-bar
+
+        menuBar = new JMenuBar();
+        fileMenu = new JMenu("File");
+        openItem = new JMenuItem("Open");
+        saveItem = new JMenuItem("Save");
+        exitItem = new JMenuItem("Exit");
+
+        openItem.addActionListener(this);
+        saveItem.addActionListener(this);
+        exitItem.addActionListener(this);
+
+        fileMenu.add(openItem);
+        fileMenu.add(saveItem);
+        fileMenu.add(exitItem);
+        menuBar.add(fileMenu);
+
+
+        //Menu-bar
+        this.setJMenuBar(menuBar);
         this.add(fontLabel);
         this.add(fontSizeSpinner);
         this.add(fontColorButton);
@@ -81,6 +117,65 @@ public class TextEditor extends JFrame implements ActionListener{
 
         if(e.getSource() == fontBox){
             textArea.setFont(new Font((String)fontBox.getSelectedItem(), Font.PLAIN,textArea.getFont().getSize()));
+        }
+
+        if(e.getSource() == openItem){
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setCurrentDirectory(new File("."));
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("Text files", "txt");
+            fileChooser.setFileFilter(filter);
+
+            int response = fileChooser.showOpenDialog(null);
+
+            if(response == JFileChooser.APPROVE_OPTION){
+                File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                Scanner fileIn = null;
+
+                try {
+                    fileIn = new Scanner(file);
+                    if(file.isFile()){
+                        while(fileIn.hasNextLine()){
+                            String line = fileIn.nextLine()+"\n";
+                            textArea.append(line);
+                        }
+                    }
+                } catch (FileNotFoundException e1) {
+                    JOptionPane.showMessageDialog(null, "Error: Could not open the file!", "File Error", JOptionPane.ERROR_MESSAGE);
+                    e1.printStackTrace();
+                } finally{
+                    fileIn.close();
+                }
+            }
+
+        }
+
+        if(e.getSource() == saveItem){
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setCurrentDirectory(new File("."));
+
+            int response = fileChooser.showSaveDialog(null);
+
+            if(response == JFileChooser.APPROVE_OPTION){
+                File file;
+                PrintWriter fileOut = null;
+
+                file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                try {
+                    fileOut = new PrintWriter(file);
+                    fileOut.println(textArea.getText());
+                } catch (FileNotFoundException e1) {
+                    JOptionPane.showMessageDialog(null, "Error: Could not save the file!", "File Error", JOptionPane.ERROR_MESSAGE);
+                    e1.printStackTrace();
+                }
+                finally{
+                    fileOut.close();
+                }
+            }
+            
+        }
+
+        if(e.getSource() == exitItem){
+            System.exit(0);
         }
     }
 }
